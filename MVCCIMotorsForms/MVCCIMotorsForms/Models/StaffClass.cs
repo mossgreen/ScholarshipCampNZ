e5c5f4563593e4beea0264ef344696526253fbe4
MossGU <gufeifei@outlook.com>
2017-02-21 15:08:15 +1300

    Add  Address2

--- MVCCIMotorsForms/MVCCIMotorsForms/Models/StaffClass.cs
+++ MVCCIMotorsForms/MVCCIMotorsForms/Models/StaffClass.cs
@@ -9,12 +9,17 @@ namespace MVCCIMotorsForms.Models
     public class StaffClass
     {
         public int StaffId { get; set; }
-        [Display(Name = "First Name")]
+
+        //display annotation will afect the view on "Home/EditStaffMember?staffId=4"
+        //@Html.LabelFor will display the name defined here
+        [Display(Name = "First--- Name")]
         public string FirstName { get; set; }
         [Display(Name = "Last Name ")]
         public string LastName { get; set; }       
         [Display(Name = "Address1  ")]
         public string Address1 { get; set; }
+        [Display(Name = "Address2  ")]
+        public string Address2 { get; set; }
         [Display(Name = "Salary")]
         public Nullable<decimal> Salary { get; set; }
     }
