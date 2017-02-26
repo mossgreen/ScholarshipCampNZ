12ca062b1d6fe2be5b96a3639fbda97324e55c14
MossGU <gufeifei@outlook.com>
2017-02-27 09:51:13 +1300

    add knockout project

---
+++ MVVMExample/MVVMExample/Models/StaffListModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVVMExample.Models
+{
+    public class StaffListModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
