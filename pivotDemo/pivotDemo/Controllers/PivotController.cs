ea9a5a9a59854510ffb08db84e7d8737aca5cbf1
Moss GU <gufeifei@outlook.com>
2017-03-13 01:53:22 +0000

    pivot jquery bug

---
+++ pivotDemo/pivotDemo/Controllers/PivotController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pivotDemo.Controllers
+{
+    public class PivotController : Controller
+    {
+        // GET: Pivot
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetChild()
+        {
+            var context = new Entities();
+            var children = context.Children.Select(c => new { c.Id, c.SchoolId, c.Name });
+            return Json(children, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
