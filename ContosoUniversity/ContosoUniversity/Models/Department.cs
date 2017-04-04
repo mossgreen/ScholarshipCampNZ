8f3a6d7ac9936da355d41a827d7ca866e2253006
MossGU <gufeifei@outlook.com>
2017-04-04 23:25:57 +1200

    complex data model

---
+++ ContosoUniversity/ContosoUniversity/Models/Department.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Department
+    {
+        public int DepartmentID { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+
+
+        /* to change SQL data type mapping so that the column 
+         * will be defined using the SQL Server money type in the database
+         * column will be holding currency amounts, and the money data type is more appropriate*/
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "money")]
+        public decimal Budget { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; }
+
+        public int? InstructorID { get; set; }
+
+        public Instructor Administrator { get; set; }
+        public ICollection<Course> Courses { get; set; }
+    }
+}
--- ContosoUniversity/ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace ContosoUniversity.Models
 {
@@ -8,9 +11,37 @@ namespace ContosoUniversity.Models
 
         //By default, EF interprets a property that's named ID or classnameID as the primary key.
         public int ID { get; set; }
+
+        /*The StringLength attribute won't prevent a user from entering white space for a name. 
+         * You can use the RegularExpression attribute to apply restrictions to the input.*/
+        [Required]
+        [StringLength(50)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
+
+        /*The DataType attribute is used to specify a data type that is more specific 
+         * than the database intrinsic type. 
+         * In this case we only want to keep track of the date, not the date and time.*/
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
 
 
         /*The Enrollments property is a navigation property. 
