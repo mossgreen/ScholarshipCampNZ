37aca3abaac8f97fe9f301e89ca7e43ef642e4bd
MossGU <gufeifei@outlook.com>
2017-04-18 18:53:12 +1200

    add controller

---
+++ SuperAPIDemo/Properties/Models/HeroGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperAPIDemo.Properties.Models
+{
+    public class HeroGenerator
+    {
+        public static string GetHeroName(string firstName, string lastName)
+        {
+            var f = firstName.Substring(0, 1);
+            var l = lastName.Substring(0, 1);
+
+            return "moss's getname"+ firstName+"haha"+lastName;
+        }
+    }
+}
---
+++ SuperAPIDemo/Properties/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperAPIDemo.Properties.Models
+{
+    public class Person
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        private string _heroName;
+
+        public string HeroName { get { return _heroName; } }
+
+        public void SetHeroName() { _heroName = HeroGenerator.GetHeroName(FirstName, LastName); }
+    }
+}
