4ff31f0185427a943d08f7dcccf2628082643d29
MossGU <gufeifei@outlook.com>
2017-04-19 21:59:17 +1200

    add knockout

--- BookService/BookService/App_Start/BundleConfig.cs
+++ BookService/BookService/App_Start/BundleConfig.cs
@@ -23,6 +23,11 @@ namespace BookService
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            // New code:
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                      "~/Scripts/knockout-{version}.js",
+                      "~/Scripts/app.js"));
         }
     }
 }
---
+++ BookService/BookService/Scripts/app.js
@@ -0,0 +1,34 @@
+﻿var ViewModel = function () {
+    var self = this;
+    self.books = ko.observableArray(); //observableArray class is the array version of observable
+    self.error = ko.observable;
+
+    var booksUri = '/api/books/';
+
+    function ajaxHelper(uri, method, data) {
+        self.error(''); //clear error message
+        return $.ajax({
+            type: method,
+            url: uri,
+            dataType: 'json',
+            contentType: 'application/json',
+            data:data?JSON.stringify(data):null
+        })
+            .fail(function (jqXHR, textStatus, errorThrown) {
+                self.error(errorThrown);
+            });
+    };
+
+    function getAllBooks() {
+        ajaxHelper(booksUri, 'GET')
+            .done(function (data) {
+                self.books(data);
+            });
+    }
+
+    //fetch the initial data
+    getAllBooks();
+
+};
+
+ko.applyBindings(new ViewModel());
