e3f058b289a7dd41cb8590508b289c6fea29a9f9
MossGU <gufeifei@outlook.com>
2017-04-19 23:08:05 +1200

    Add UI

--- BookService/BookService/Scripts/app.js
+++ BookService/BookService/Scripts/app.js
@@ -1,34 +1,71 @@
 ﻿var ViewModel = function () {
     var self = this;
-    self.books = ko.observableArray(); //observableArray class is the array version of observable
-    self.error = ko.observable;
+    self.books = ko.observableArray();
+    self.error = ko.observable();
+    self.detail = ko.observable();
+    self.authors = ko.observableArray();
+    self.newBook = {
+        Author: ko.observable(),
+        Genre: ko.observable(),
+        Price: ko.observable(),
+        Title: ko.observable(),
+        Year: ko.observable()
+    }
+
 
     var booksUri = '/api/books/';
+    var authorsUri = '/api/authors/';
+
 
     function ajaxHelper(uri, method, data) {
-        self.error(''); //clear error message
+        self.error(''); // Clear error message
         return $.ajax({
             type: method,
             url: uri,
             dataType: 'json',
             contentType: 'application/json',
-            data:data?JSON.stringify(data):null
-        })
-            .fail(function (jqXHR, textStatus, errorThrown) {
-                self.error(errorThrown);
-            });
-    };
-
-    function getAllBooks() {
-        ajaxHelper(booksUri, 'GET')
-            .done(function (data) {
-                self.books(data);
-            });
+            data: data ? JSON.stringify(data) : null
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            self.error(errorThrown);
+        });
     }
 
-    //fetch the initial data
-    getAllBooks();
+    function getAllBooks() {
+        ajaxHelper(booksUri, 'GET').done(function (data) {
+            self.books(data);
+        });
+    }
 
+    function getAuthors() {
+        ajaxHelper(authorsUri, 'GET').done(function (data) {
+            self.authors(data);
+        });
+    }
+    
+    self.getBookDetail = function (item) {
+        ajaxHelper(booksUri + item.Id, 'GET').done(function (data) {
+            self.detail(data);
+        });
+    }
+
+    self.addBook = function (formElement) {
+        var book = {
+            AuthorId: self.newBook.Author().Id,
+            Genre: self.newBook.Genre(),
+            Price: self.newBook.Price(),
+            Title: self.newBook.Title(),
+            Year: self.newBook.Year()
+        };
+
+        ajaxHelper(booksUri, 'POST', book).done(function (item) {
+            self.books.push(item);
+        });
+    }
+
+    getAuthors();
+
+    // Fetch the initial data.
+    getAllBooks();
 };
 
 ko.applyBindings(new ViewModel());
