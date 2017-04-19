15b16bb067d8a1b9a10109129745bdca3a93199d
MossGU <gufeifei@outlook.com>
2017-04-19 20:51:59 +1200

    initialize api project

---
+++ BookServiceAPI/BookServiceAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace BookServiceAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ BookServiceAPI/BookServiceAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BookServiceAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
