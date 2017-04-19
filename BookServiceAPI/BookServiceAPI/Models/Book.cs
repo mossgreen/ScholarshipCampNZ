d6ee7a463b0ea2893f98d119c4bd135d86ee0fee
MossGU <gufeifei@outlook.com>
2017-04-19 21:11:14 +1200

    add api project, models and controller, EF

---
+++ BookServiceAPI/BookServiceAPI/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookServiceAPI.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+        public string Genre { get; set; }
+
+        //foreign key
+        public int AuthorId { get; set; }
+
+        //navigation property
+        public Author Author { get; set; }
+    }
+}
